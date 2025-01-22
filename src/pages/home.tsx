6746a3743b3432fa5179703f6dde78532e3b631f
clalhruaizela <150893745+clalhruaizela@users.noreply.github.com>
2025-01-22 17:13:14 +0530

    Add files via upload

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="bg-black text-white w-full h-screen ">
+      <div>home</div>
+      <div>
+        <Button>Home</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
